41b44ff7af7070258b1534efb57a5d7f70d7abe0
ligr00vefe <ligr00vefe@naver.com>
2020-12-09 05:28:57 +0900

    2020/12/07 NodeJS 미들웨어(express, nodemon) 활용

--- Ch02/server.js
+++ Ch02/server.js
@@ -1,14 +1,22 @@
 // express -> NodeJS의 웹 어플리케이션 프레임워크
 
-const express = require('express');
+const express = require("express");
 const server = express();
 
-server.get('/', (req, res)=> {
+// 미들웨어 생성(use)
+server.use(express.static(__dirname + "/img"));
 
+server.use((req, res, next)=>{
+    // config
+    next();
+});
+
+server.get("/", (req, res)=> {
     // __dirname  -> root 경로이름
     // __filename -> 파일이름
     // statusCode(수신 상태 코드) -> 200 수신 양호
-    res.statusCode(200).sendFile(__dirname + '/Ch02/index.html');
+    // status로 변환
+    res.status(200).sendFile(__dirname + "/index.html");
 })
 
 server.listen(3000, ()=> {
@@ -19,3 +27,4 @@ server.listen(3000, ()=> {
 // 서버가 업데이트 될 경우 자동으로 재시작해주는 라이브러리
 // npm i -g nodemon
 // i(install), -g(global)
+// 실행문: nodemon server
